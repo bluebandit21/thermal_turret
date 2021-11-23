66d3fa2323a5184a2390cfb4fd637c59202e2305
Santa-Bunny <AJEKAKENT@gmail.com>
2021-11-23 00:24:40 -0500

    Added serLCD Setup

--- microcontroller/Core/Src/main.c
+++ microcontroller/Core/Src/main.c
@@ -72,7 +72,89 @@ static void MX_TIM1_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+#define DISPLAY_ADDRESS1 0x72 //This is the default address of the OpenLCD
+#define MAX_ROWS 4
+#define MAX_COLUMNS 20
 
+//OpenLCD command characters
+#define SPECIAL_COMMAND 254  //Magic number for sending a special command
+#define SETTING_COMMAND 0x7C //124, |, the pipe character: The command to change settings: baud, lines, width, backlight, splash, etc
+
+//OpenLCD commands
+#define CLEAR_COMMAND 0x2D					//45, -, the dash character: command to clear and home the display
+#define CONTRAST_COMMAND 0x18				//Command to change the contrast setting
+#define ADDRESS_COMMAND 0x19				//Command to change the i2c address
+#define SET_RGB_COMMAND 0x2B				//43, +, the plus character: command to set backlight RGB value
+#define ENABLE_SYSTEM_MESSAGE_DISPLAY 0x2E  //46, ., command to enable system messages being displayed
+#define DISABLE_SYSTEM_MESSAGE_DISPLAY 0x2F //47, /, command to disable system messages being displayed
+#define ENABLE_SPLASH_DISPLAY 0x30			//48, 0, command to enable splash screen at power on
+#define DISABLE_SPLASH_DISPLAY 0x31			//49, 1, command to disable splash screen at power on
+#define SAVE_CURRENT_DISPLAY_AS_SPLASH 0x0A //10, Ctrl+j, command to save current text on display as splash
+
+// special commands
+#define LCD_RETURNHOME 0x02
+#define LCD_ENTRYMODESET 0x04
+#define LCD_DISPLAYCONTROL 0x08
+#define LCD_CURSORSHIFT 0x10
+#define LCD_SETDDRAMADDR 0x80
+
+// flags for display entry mode
+#define LCD_ENTRYRIGHT 0x00
+#define LCD_ENTRYLEFT 0x02
+#define LCD_ENTRYSHIFTINCREMENT 0x01
+#define LCD_ENTRYSHIFTDECREMENT 0x00
+
+// flags for display on/off control
+#define LCD_DISPLAYON 0x04
+#define LCD_DISPLAYOFF 0x00
+#define LCD_CURSORON 0x02
+#define LCD_CURSOROFF 0x00
+#define LCD_BLINKON 0x01
+#define LCD_BLINKOFF 0x00
+
+// flags for display/cursor shift
+#define LCD_DISPLAYMOVE 0x08
+#define LCD_CURSORMOVE 0x00
+#define LCD_MOVERIGHT 0x04
+#define LCD_MOVELEFT 0x00
+
+
+uint8_t _i2cAddr = DISPLAY_ADDRESS1;
+uint8_t _displayControl = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
+uint8_t _displayMode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
+
+
+/* beginTransmission();
+  transmit(SPECIAL_COMMAND);                      //Send special command character
+  transmit(LCD_DISPLAYCONTROL | _displayControl); //Send the display command
+  transmit(SPECIAL_COMMAND);                      //Send special command character
+  transmit(LCD_ENTRYMODESET | _displayMode);      //Send the entry mode command
+  transmit(SETTING_COMMAND);                      //Put LCD into setting mode
+  transmit(CLEAR_COMMAND);                        //Send clear display command
+  endTransmission();                              //Stop transmission
+  delay(50);                                      //let things settle a bit
+ */
+
+/*
+ *
+ *
+*/
+void setup_LCD(){
+	uint8_t buf[12];
+	buf[0] = SPECIAL_COMMAND;
+	buf[2] = LCD_DISPLAYCONTROL | _displayControl;
+	buf[4] = SPECIAL_COMMAND;
+	buf[6] = LCD_ENTRYMODESET | _displayMode;
+	buf[8] = SETTING_COMMAND;
+	buf[10] = CLEAR_COMMAND;
+	ret = HAL_I2C_Master_Transmit(&hi2c1, ACC_W_ADDR, buf, 12, HAL_MAX_DELAY);
+
+	if (ret != HAL_OK){
+		  strcpy((char*)buf, "No Write\r\n");
+		  HAL_UART_Transmit(&hlpuart1, buf, strlen((char*)buf), HAL_MAX_DELAY);
+	}
+
+}
 
 /* void target_gimbal_angles(short yaw, short pitch)
  *  Moves gimbal to specified position of yaw and pitch.
@@ -110,6 +192,22 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
+
+
+  // 5 inputs from Jetson Nano
+  //- Up
+  //- Down
+  //- Left
+  //- Right
+  //- Laser
+
+  // 2 PWM Outputs
+
+  // 2 I2C I/O
+  // Temp Sens
+  // Display
+
+  // ADC for touch
 
   /* USER CODE END Init */
 
--- misc/ID_n_Temp_Get.ino/ID_n_Temp_Get.ino.ino
+++ misc/ID_n_Temp_Get.ino/ID_n_Temp_Get.ino.ino
@@ -22,34 +22,76 @@ SparkFun IR Thermometer Evaluation Board - MLX90614
 ******************************************************************************/
 
 #include <Wire.h> // I2C library, required for MLX90614
-#include <SparkFunMLX90614.h>//Click here to get the library: http://librarymanager/All#Qwiic_IR_Thermometer by SparkFun
+#include <SparkFunMLX90614.h> //Click here to get the library: http://librarymanager/All#Qwiic_IR_Thermometer by SparkFun
+#include <SerLCD.h> //Click here to get the library: http://librarymanager/All#SparkFun_SerLCD
 
+
+SerLCD lcd; // Initialize the library with default I2C address 0x72
 IRTherm therm; // Create an IRTherm object to interact with throughout
 
 void setup() 
 {
   Serial.begin(115200); // Initialize Serial to log output
-  Wire.begin(); //Join I2C bus
+  Wire.begin(); //Joing I2C bus
   
-  if (therm.begin() == false){ // Initialize the MLX90614
+  if (therm.begin() == false){ // Initialize thermal IR sensor
     Serial.println("Qwiic IR thermometer did not acknowledge! Freezing!");
     while(1);
   }
-  Serial.println("Qwiic IR thermometer acknowledged.");
+  Serial.println("Qwiic IR Thermometer did acknowledge.");
   
-  if (therm.readID()) // Read from the ID registers
-  { // If the read succeeded, print the ID:
-    Serial.println("ID: 0x" + 
-                   String(therm.getIDH(), HEX) +
-                   String(therm.getIDL(), HEX));
-  }
-  Serial.println(String(therm.readEmissivity()));
+  therm.setUnit(TEMP_F); // Set the library's units to Farenheit
+  // Alternatively, TEMP_F can be replaced with TEMP_C for Celsius or
+  // TEMP_K for Kelvin.
+  
+  pinMode(LED_BUILTIN, OUTPUT); // LED pin as output
+
+  //LCD
+  lcd.begin(Wire); //Set up the LCD for I2C communication
+
+  lcd.setBacklight(255, 255, 255); //Set backlight to bright white
+  lcd.setContrast(0); //Set contrast. Lower to 0 for higher contrast.
+
+  lcd.clear(); //Clear the display - this moves the cursor to home position as well
+  lcd.print("Hello");
+
+  
 }
-
-
-
 
 void loop() 
 {
+  digitalWrite(LED_BUILTIN, HIGH);
+    
+  // Call therm.read() to read object and ambient temperatures from the sensor.
+  if (therm.read()) // On success, read() will return 1, on fail 0.
+  {
+    // Use the object() and ambient() functions to grab the object and ambient
+  // temperatures.
+  // They'll be floats, calculated out to the unit you set with setUnit().
+    Serial.print("Object: " + String(therm.object(), 2));
+    Serial.println("F");
+    Serial.print("Ambient: " + String(therm.ambient(), 2));
+    Serial.println("F");
+    Serial.println();
 
+  // LCD
+  // Set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  
+  if (therm.object() > 77){
+    lcd.setFastBacklight(255, 0, 0); //Set backlight to bright white
+  }
+  else {
+    lcd.setFastBacklight(0,255,0);
+  }
+  lcd.setCursor(0, 1);
+  // Print the number of seconds since reset:
+  lcd.print("Temperature: " + String(therm.object(), 2) + "F");
+  lcd.setCursor(0,0);
+  lcd.print("Ambient: " + String(therm.ambient(), 2) + "F");
+
+    
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
 }

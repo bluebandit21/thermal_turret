7f77df12e4aff9d2321dd98578682f9136e78904
Santa-Bunny <AJEKAKENT@gmail.com>
2021-11-25 15:51:34 -0500

    Added MLX Lib

---
+++ microcontroller/Core/Src/MLX90614.cpp
@@ -0,0 +1,24 @@
+/*
+ * MLX90614.cpp
+ * Source file for the SparkFun IR Thermometer Library
+ *
+ * By: Andrew Kent
+ *
+ * This Library is based on:
+ * SparkFunMLX90614.cpp
+ * The SparkFun IR Thermometer Library
+ *
+ * Jim Lindblom @ SparkFun Electronics
+ * October 23, 2015
+ * https://github.com/sparkfun/SparkFun_MLX90614_Arduino_Library
+ *
+ * This file defines the SMBus hardware interface(s) for the MLX90614 IR thermometer
+ * and abstracts temperature measurments and other features of the MLX90614
+ *
+ * Development environment specifics:
+ * SparkFun IR Thermometer Evaluation Board - MLX90614
+ */
+
+#include "main.h"
+#include "float.h"
+
